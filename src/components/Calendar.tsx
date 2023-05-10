5d040b4232022155eda544da8c50eb05893e20e3
Raiane <raiane.oliveira404@gmail.com>
2023-05-10 16:28:54 -0300

    feat: display last and first days of months

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,17 +1,38 @@
-import { format, getDay, getMonth, isSameDay, isWeekend } from "date-fns";
+import {
+  format,
+  getDay,
+  getMonth,
+  isSameDay,
+  isWeekend,
+  lastDayOfMonth,
+  lastDayOfWeek,
+} from "date-fns";
 import { useCalendar } from "../context/DatesContext";
-import { useState } from "react";
+import React, { useState } from "react";
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
+
+interface IDayProps {
+  index: number;
+  day: Date;
+  firstDayOfWeekIndex?: number;
+  isAnotherMonth?: boolean;
+}
 
 export function Calendar() {
   const { calendar } = useCalendar();
+
+  // The initial state setting is the current month
   const [monthIndex, setMonthIndex] = useState(getMonth(calendar.currentDay));
 
-  const year = format(calendar.currentDay, "yyyy");
+  const year = Number(format(calendar.currentDay, "yyyy"));
   const month = format(new Date(Number(year), monthIndex, 1), "MMMM");
 
-  const firstDayOfWeekIndex = getDay(new Date(Number(year), monthIndex, 1)) + 1;
-  console.log(firstDayOfWeekIndex);
+  const firstDayOfWeekIndex = getDay(new Date(year, monthIndex, 1));
+  const lastDayMonth = lastDayOfMonth(new Date(year, monthIndex, 1));
+  const lastDayOfWeekIndex = getDay(lastDayMonth);
+
+  const listPreviousDays = renderPreviousDays();
+  const listNextDays = renderNextDays();
 
   function onNextMonth() {
     setMonthIndex(monthIndex + 1);
@@ -29,6 +50,25 @@ export function Calendar() {
     if (monthIndex === 0) {
       setMonthIndex(11);
     }
+  }
+
+  function renderPreviousDays() {
+    const previousMonth = calendar.months[monthIndex - 1];
+
+    // Copies the last days of the previous month based on the remaining weekdays
+    const list = previousMonth.slice(
+      previousMonth.length - firstDayOfWeekIndex
+    );
+    return list;
+  }
+
+  function renderNextDays() {
+    const nextMonth = calendar.months[monthIndex + 1];
+
+    // Copies the first days of the next month based on the remaining weekdays
+    const restOfWeek = 6 - lastDayOfWeekIndex;
+    const list = nextMonth.slice(0, restOfWeek);
+    return list;
   }
 
   return (
@@ -54,29 +94,62 @@ export function Calendar() {
           ))}
         </header>
         <div className="grid grid-flow-row grid-cols-7 p-0.5 gap-0.5 bg-black-border">
-          {calendar.months[monthIndex].map((day: Date, index: number) => {
-            const isFirstDay = index === 0;
-            const isDayWeekend = isWeekend(day);
-            return (
-              <div
-                key={index}
-                className={`grid p-2 w-full h-44 ${
-                  isFirstDay && " col-start-" + firstDayOfWeekIndex.toString()
-                } ${isDayWeekend ? " bg-black-dark" : "bg-black"}`}
-              >
-                <span
-                  className={`justify-self-end font-semibold ${
-                    isSameDay(day, calendar.currentDay) &&
-                    "bg-blue p-2 rounded-full h-max"
-                  }`}
-                >
-                  {format(day, "d")}
-                </span>
-              </div>
-            );
-          })}
+          {listPreviousDays.map((day: Date, index: number) => (
+            <DayCalendar
+              day={day}
+              index={index}
+              isAnotherMonth={true}
+              key={index}
+            />
+          ))}
+          {calendar.months[monthIndex].map((day: Date, index: number) => (
+            <DayCalendar
+              day={day}
+              index={index}
+              firstDayOfWeekIndex={firstDayOfWeekIndex + 1}
+              key={index}
+            />
+          ))}
+          {listNextDays.map((day: Date, index: number) => (
+            <DayCalendar
+              day={day}
+              index={index}
+              isAnotherMonth={true}
+              key={index}
+            />
+          ))}
         </div>
       </main>
     </div>
   );
 }
+
+const DayCalendar: React.FC<IDayProps> = ({
+  day,
+  index,
+  firstDayOfWeekIndex,
+  isAnotherMonth = false,
+}) => {
+  const { calendar } = useCalendar();
+  const isFirstDay = index === 0;
+  const isDayWeekend = isWeekend(day);
+
+  return (
+    <div
+      className={`grid p-2 w-full h-44 ${
+        isFirstDay && " col-start-" + firstDayOfWeekIndex
+      } ${isDayWeekend ? " bg-black-dark" : "bg-black"}
+        ${isAnotherMonth && " opacity-60"}
+      `}
+    >
+      <span
+        className={`justify-self-end font-semibold ${
+          isSameDay(day, calendar.currentDay) &&
+          "bg-blue p-2 rounded-full h-max"
+        }`}
+      >
+        {format(day, "d")}
+      </span>
+    </div>
+  );
+};
